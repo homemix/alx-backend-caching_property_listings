712836c7e1a39753581b7200c4e078b79a029b4e
homemix <kenmutati@gmail.com>
2025-07-12 06:16:43 +0300

    EDIT: docker compose for postgres and redis.

--- properties/models.py
+++ properties/models.py
@@ -6,7 +6,7 @@ class Property(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
     location = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
