91a7b5b5693bf4e9d20c71b0d8fab8aadac75ece
homemix <kenmutati@gmail.com>
2025-07-12 05:43:07 +0300

    ADD: database config and migration

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Property(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    location = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
